d435052c846ed87dda1ede1092c8072bf1a42ba8
Raphaël Doré <rdore@neomailbox.ch>
2015-08-27 21:35:56 -0400

    Only display a summary of the offer instead of everything. New StringHelper class to help format string properly for display.

---
+++ SuperOldCars.Web/Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperOldCars.Web.Helpers
+{
+    public static class StringHelper
+    {
+        public static string FormatMoney(this HtmlHelper html, decimal amount)
+        {
+            return string.Format("{0:c}", amount);
+        }
+    }
+}
