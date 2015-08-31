6fb6796012f2ed15dd8edd0acb0cf90b22d78595
Raphaël Doré <rdore@neomailbox.ch>
2015-08-31 12:59:02 -0400

    Ajout thème bootstrap. La page create utilise maintenant des inputs bootstrap. On valide maintenant comme du monde l'année de la voiture (server side). Et encore plus! Gros commit car j'étais trop lâche....

--- SuperOldCars.Web/App_Start/BundleConfig.cs
+++ SuperOldCars.Web/App_Start/BundleConfig.cs
@@ -16,16 +16,21 @@ namespace SuperOldCars.Web
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/globalize.js"));
 
             // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
             // prêt pour la production, utilisez l'outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //            "~/Content/bootstrap.css",
+            //            "~/Content/SuperOldCars.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap.css",
-                        "~/Content/SuperOldCars.css"));
+            "~/Content/themes/bootstrap_flatly.css",
+            "~/Content/SuperOldCars.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
---
+++ SuperOldCars.Web/Helpers/InputHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperOldCars.Web.Helpers
+{
+    public static class InputHelper
+    {
+        public static IEnumerable<SelectListItem> GetYearRangeList(this HtmlHelper html, int numberOfYears)
+        {
+            List<SelectListItem> years = new List<SelectListItem>();
+
+            DateTime currentDate = DateTime.Now;
+
+            for (int i = currentDate.Year; i >= currentDate.AddYears(-numberOfYears).Year; i--)
+            {
+                years.Add(new SelectListItem
+                {
+                    Text = i.ToString(),
+                    Value = i.ToString()
+                } );
+            }
+
+            return years;
+
+        }
+    }
+}
